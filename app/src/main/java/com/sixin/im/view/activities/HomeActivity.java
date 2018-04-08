5921fbded1baa5a4972e81a988dac7a2fa888092
CUTYlife <2055305346@qq.com>
2018-04-08 22:21:27 +0800

    项目目录结构的搭建

---
+++ app/src/main/java/com/sixin/im/view/activities/HomeActivity.java
@@ -0,0 +1,15 @@
+package com.sixin.im.view.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.sixin.im.R;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
---
+++ app/src/main/java/com/sixin/im/view/activities/SplashActivity.java
@@ -0,0 +1,15 @@
+package com.sixin.im.view.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.sixin.im.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
