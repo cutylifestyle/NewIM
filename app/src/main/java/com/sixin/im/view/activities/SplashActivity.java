6e5a3616598aca6a17f8487b125b4e17f910b9de
20553 <2055305346@qq.com>
2018-04-09 23:50:51 +0800

    欢迎页面搭建

--- app/src/main/java/com/sixin/im/view/activities/SplashActivity.java
+++ app/src/main/java/com/sixin/im/view/activities/SplashActivity.java
@@ -2,6 +2,8 @@ package com.sixin.im.view.activities;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
+import android.view.WindowManager;
 
 import com.sixin.im.R;
 
@@ -9,6 +11,11 @@ public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        // TODO: 2018/4/9 自定义view，修改字体
+        // TODO: 2018/4/9 低版本手机的兼容性测试
+        // TODO: 2018/4/9 界面显示比例是否正常
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
     }
